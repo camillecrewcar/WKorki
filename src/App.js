e8aef89b6788c42b6df3a2bece2031cb5f39a98d
kamil <camille.crewcar@gmail.com>
2024-09-15 23:19:07 +0200

    adding subjects

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import * as React from 'react';
 import ResponsiveAppBar from './components/global/nav';
 import './App.css';
 import HelloPost from './components/mainPage/helloPost';
+import Subject from './components/mainPage/subject';
+
 import { Container } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material/styles';
+import Grid from '@mui/material/Grid2';
 
 
 
@@ -36,6 +39,30 @@ export default function App() {
             Dołącz do nas i czerp przyjemność z nauki!
           </Typography>
           <HelloPost></HelloPost>
+          <Typography
+            variant="h4"
+            textAlign="center"
+            fontWeight={3000}
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              color: 'black',
+            }}
+          >
+            Przedmioty których uczymy:
+          </Typography>
+          <Grid container spacing={2}>
+            <Grid size={2}>
+              <Subject>
+
+              </Subject>
+            </Grid>
+            <Grid size={4}>
+            </Grid>
+            <Grid size={4}>
+            </Grid>
+            <Grid size={8}>
+            </Grid>
+          </Grid>
         </Container>
       </div>
     </ThemeProvider>
---
+++ src/components/mainPage/subject.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+
+
+function Subject() {
+    return(
+        <Paper elevation={3} sx={{width: 200 , height: 200}}>
+            dwadwad
+        </Paper>
+    )
+}
+export default Subject;
