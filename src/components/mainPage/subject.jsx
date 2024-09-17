8911a0341f7703cd508ec0fc50b37953ae9f319e
kamil <camille.crewcar@gmail.com>
2024-09-17 13:14:25 +0200

    static version of sublects

--- src/components/mainPage/subject.jsx
+++ src/components/mainPage/subject.jsx
@@ -1,12 +1,35 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CalculateRoundedIcon from '@mui/icons-material/CalculateRounded';
 
-
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#E49378'
+      }
+    }
+  });
 function Subject() {
     return(
-        <Paper elevation={3} sx={{width: 200 , height: 200}}>
-            dwadwad
-        </Paper>
+
+        <ThemeProvider theme={theme}>
+            
+            <Paper elevation={3} 
+            sx={{width: 200,
+                height: 200,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                border: 4,
+                borderColor: 'primary.main',
+                flexDirection: 'column' }}>
+                <CalculateRoundedIcon sx={{ fontSize: 120, color: "primary.main"}}></CalculateRoundedIcon>
+                <h3>
+                    Matematyka
+                </h3>
+            </Paper>
+        </ThemeProvider>
     )
 }
 export default Subject;
